8b92a2012d0a1a644050532b21dfa7f7c5c4c704
Alex Scherer <alex.scherer@outlook.com>
2021-08-23 14:13:39 +0200

    made gallery cards clickable

--- src/js/modules/card-links.js
+++ src/js/modules/card-links.js
@@ -1,9 +1,16 @@
+
 $(function() {
     
+    
     $('.uk-card').on('click', function(e) {
-        let cardLink = $(this).find('a.card-link').attr('href');
-        if (cardLink !== undefined) {
-            window.location = cardLink;
+        if ($(this).hasClass('lightbox')) {
+            UIkit.lightbox('.gallery-container').show($(this).parent().index());
+
+        } else {
+            let cardLink = $(this).find('a.card-link').attr('href');
+            if (cardLink !== undefined) {
+                window.location = cardLink;
+            }
         }
         return false;
     });
