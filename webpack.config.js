2bc935c51e7755c1c6ce30de3756dabb8e596589
Alex Scherer <alex.scherer@outlook.com>
2021-08-23 15:33:46 +0200

    setup webpack for js and scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const projectPaths = {
+    projectDir:         __dirname,
+    projectJsPath:      path.resolve(__dirname, 'src/js'),
+    projectScssPath:    path.resolve(__dirname, 'src/scss')
+}
 
 module.exports = {
-    entry: './src/js/main.js',
+    entry: {
+        'theme-pack': './src/js/main.js',
+        'theme-green': './src/scss/main.scss',
+        'theme-pink': './src/scss/main-pink.scss'
+    },
     output: {
-        path: path.resolve(__dirname, 'static', 'js'),
-        filename: 'main.bundle.js'
-    }
+        path: path.resolve(__dirname),
+        filename: './static/js/[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(sass|scss)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: './static/css/[name].css'
+        }),
+    ]
 }
